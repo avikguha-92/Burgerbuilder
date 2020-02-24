ee77e0413a3b4df30d52b3668edd10f5bb651acf
avikguha-92 <60727799+avikguha-92@users.noreply.github.com>
2020-02-24 17:42:16 +0530

    Burger
    Burger

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Classes from "./Modal.module.css";
-import Auxiliary from "../../../hoc/Auxiliary";
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
 const modal = props => (
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Auxiliary from "../../hoc/Auxiliary";
+import Auxiliary from "../Auxiliary/Auxiliary";
 import Classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/NavigationItems/SideDrawer/SideDrawer";
+import SideDrawer from "../../components/Navigation/NavigationItems/SideDrawer/SideDrawer";
 class Layout extends Component {
   state = {
     showSideDrawer: false
